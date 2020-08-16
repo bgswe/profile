04341bca1b6297ee2119a274c5eb6042fcaa537e
Bryce Gallion <bg@Bryces-MacBook-Pro.local>
2020-08-16 09:56:20 -0700

    Includes pug integration w/ reactivity

--- src/components/subheader.ts
+++ src/components/subheader.ts
@@ -1,39 +1,111 @@
 import Component from 'lib/component'
 import * as pug from 'pug'
 
+function debounc(func, delta) {
+    let timeout: NodeJS.Timeout
+
+    return function (...args: any[]) {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => func.apply(this, args), delta)
+    }
+}
 
 function CompFactory(config?: any) {
     const { name, templateFunc } = config
 
     return function Comp<T extends { new(...args: any[]): {} }>(con: T) {
         return class extends con {
-            _templateEl: HTMLElement
+            _templateEl: HTMLTemplateElement
             _el: HTMLElement
+
+            _renderTimeout: NodeJS.Timeout
 
             constructor(...args: any[]) {
                 super(args)
 
-                this._templateEl = document.createElement('div')
+                const self = this
+
+                for (const p in this.data) {
+                    this.data[`_${p}`] = this.data[p]
+
+                    Object.defineProperty(this.data, p, {
+                        get() {
+                            return this[`_${p}`]
+                        },
+                        set(val) {
+                            this[`_${p}`] = val
+                            self.render()
+                        }
+                    })
+
+                }
+
+                this._templateEl = document.createElement('template')
                 this.render()
+
+                this.identifyEvents(this._el)
             }
 
             render() {
-                const rendTemp = templateFunc({})
+                this._templateEl.innerHTML = templateFunc(this.data)
 
-                if (this._el) {
-                    this._el = document.
-                } else {
-                    document.createElement()
+                const compiledNode = <HTMLElement>this._templateEl.content.cloneNode(true)
+                const newEl = <HTMLElement>compiledNode.firstChild
+
+                if (this._el)
+                    this._el.parentNode.replaceChild(newEl, this._el)
+
+                this._el = newEl
+                this.identifyEvents(this._el)
+            }
+
+            identifyEvents(element: HTMLElement) {
+                const self = this
+
+                if (!(element instanceof HTMLUnknownElement)) {
+                    const { event } = element.dataset
+                    if (event) {
+                        const { type, func } = self.methods[event]
+                        element.addEventListener(type, func)
+                    }
                 }
-                this.
+
+                Array.from(element.children).forEach(this.identifyEvents.bind(this))
+            }
+
+            get el() {
+                return this._el
             }
         }
     }
 }
+
+
+function methodFactory(data) {
+    return function method(event, callback) {
+        return { type: event, func: () => callback(data) }
+    }
+}
+
+
 
 @CompFactory({
     name: 'Subheader',
     templateFunc: require('./subheader.pug')
 })
 // Test class for a custom defined component
-export default class Subheader { constructor() { } }
+export default class Subheader {
+    data = {
+        count: 0,
+    }
+
+    method = methodFactory(this.data)
+    methods = {
+        increment: this.method('click', function (data) {
+            data.count++
+        }),
+        reset: this.method('click', function (data) {
+            data.count = 0
+        })
+    }
+}
--- src/lib/app.ts
+++ src/lib/app.ts
@@ -3,7 +3,7 @@ import Component from 'lib/component'
 
 /** An Application Component for new projects w/ top-level component specific features */
 export default class App extends Component {
-    constructor(template: string, components?: { [key: string]: { new(): Component } }) {
+    constructor(template: string, components?: { [key: string]: { new(): any } }) {
         super('App', template, components)
 
         // Append root component el to the document body
